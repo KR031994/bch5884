eaa8f8c528fbfa8ae4368c9f2d609d2038ec4a73
KR031994 <70907774+KR031994@users.noreply.github.com>
2020-10-17 13:09:34 +0530

    Update finalise.py

--- Python Projects/Finalise/finalise.py
+++ Python Projects/Finalise/finalise.py
@@ -14,7 +14,6 @@ for line in lines:
     words[1]=int(words[1]);words[5]=int(words[5])
     words[6]=float(words[6]);words[7]=float(words[7]);words[8]=float(words[8]);words[9]=float(words[9]);words[10]=float(words[10])
     words2.append(words)    
-#file.write("%-6s%5d %-4s%5s %s%-4d" % (line[0], line[1], line[2], line[3], line[4], line[5]))
 print (words2)
 #Using formatted strings to write the new pdb file
 f1=open("sample.pdb","w")
