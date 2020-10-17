122417fb8ceb48c5ead6311e5219a9928bc9c26e
KR031994 <70907774+KR031994@users.noreply.github.com>
2020-10-18 02:20:25 +0530

    read in and write out a pdb file

--- Python Projects/Finalise/finalise.py
+++ Python Projects/Finalise/finalise.py
@@ -4,8 +4,6 @@ import sys
 pdbname=sys.argv[1]
 f=open(pdbname)
 words2=[]
-words3=list()
-str1=""
 lines=f.readlines()
 
 #Typecasting each element 
@@ -15,17 +13,10 @@ for line in lines:
     words[6]=float(words[6]);words[7]=float(words[7]);words[8]=float(words[8]);words[9]=float(words[9]);words[10]=float(words[10])
     words2.append(words)    
 print (words2)
+
 #Using formatted strings to write the new pdb file
 f1=open("sample.pdb","w")
-c=0
-for line in lines:
-    
-    words3 = line.split()
-    words3[6]=str(words3[6]);words3[7]=str(words3[7]);words3[8]=str(words3[8])
-    
-    str1 = ' '.join(map(str, words3))
-
-    f1.write(str1)
-    f1.write('\n')
-     
-print ("Done")
+for line in words2:
+    f1.write("%-6s%5d %-4s%3s %-1s%4d %8.3f%8.3f%8.3f%6.2f%6.2f %-2s \n" %(line[0],int(line[1]),line[2],line[3],line[4],int(line[5]),float(line[6]),float(line[7]),float(line[8]),float(line[9]),float(line[10]),line[11]))
+f1.close()
+print ("Done!")
